336f6e91d5a1ef83f7470fc25f65bad8b97e228a
kchoudhu <kchoudhu@kchoudhus-MacBook-Pro.local>
2017-01-15 22:56:26 -0800

    Tag 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='openarc',
-      version='0.1.0',
+      version='0.2.0',
       description='Functional reactive graph backed by PostgreSQL',
       classifiers=[
         'Development Status :: 3 - Alpha',
