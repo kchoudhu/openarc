40dcb046ae8c667f18343bd7b3646a39b5c5001d
kchoudhu <kamil.choudhury@anserinae.net>
2019-10-18 10:15:27 +0000

    [#50] Add simple timing device to openarc.time

--- openarc/time.py
+++ openarc/time.py
@@ -1,6 +1,12 @@
-__all__ = [ "OATime" ]
+__all__ = [
+    "OATime",
+    "OATimer"
+]
 
-import time as coretime
+import datetime as coretime
+import time
+
+from ._env import oalog
 
 class OATime(object):
     """Executes time queries on database, returning
@@ -25,3 +31,17 @@ class OATime(object):
     class SQL(object):
         get_current_time =\
             "select now() at time zone 'utc'"
+
+class OATimer:
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
+        suffix = f"- [{self.identifier}]" if self.identifier is not None else str()
+        oalog.info(f"{self.interval*1000:>5.0f} {suffix}")
+
+    def __init__(self, identifier=None):
+        self.identifier = identifier
